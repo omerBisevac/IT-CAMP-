15d02f222aacc1a2c51069e0a51174d8b1ddf2a4
omer705 <omer.bisevac22@hotmail.com>
2021-12-15 22:24:39 +0100

    jsdom

---
+++ vezba54/vezba54.js
@@ -0,0 +1,32 @@
+// var red = document.getElementById("red");
+// console.log(red);
+// var black = document.getElementById("black");
+// console.log(black);
+
+// red.addEventListener("click", () => {
+//    document.getElementById("same3").style.color = "red";
+// })
+
+var scnd = document.getElementById("grayy");
+scnd.addEventListener("click",() => {
+    document.getElementById("container").style.backgroundColor = "gray";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// red.addEventListener("click",() => {
+//     document.getElementsByClassName("same").style.color = "red";
+    
+// })
+// black.addEventListener("click",()=> {
+//     document.getElementsByClassName("same").style.color = "black";
+// })
